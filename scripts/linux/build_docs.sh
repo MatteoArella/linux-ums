e2decc399e1d728d889c19d17307c306a40f8146
Arella Matteo <arella.matteo.95@gmail.com>
2021-09-27 22:05:34 +0200

    Deploy documentation
    Add a job to github actions for automatically deploy the package
    documentation.
    The deploy is executed only when pushing new tag.

---
+++ scripts/linux/build_docs.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+set -e
+if [ ! -z "${CI+x}" ] || [ "$CI" = "true" ]; then
+  set -x
+fi
+
+BASEDIR=$(dirname $(dirname $(dirname $(realpath "$0"))))
+ME=$(basename "$0")
+
+usage() {
+  cat <<EOF
+Usage: $ME [OPTIONS]
+
+Options:
+    -o BUILD-DIR          use BUILD-DIR as output build directory
+    -p PUBLISH-DIR        use PUBLISH-DIR as publish directory
+    -h                    show this help and exit
+EOF
+}
+
+BUILDDIR=$BASEDIR/build
+PUBLISHDIR=$BASEDIR/public
+
+while getopts ":ho:p:" opt; do
+  case "$opt" in
+  h)
+    usage
+    exit 0
+    ;;
+  o) BUILDDIR=$OPTARG ;;
+  p) PUBLISHDIR=$OPTARG ;;
+  \?)
+    echo "unknown option $OPTARG!" >&2
+    usage >&2
+    exit 1
+    ;;
+  :)
+    echo "missing value for option [-$OPTARG]" >&2
+    usage >&2
+    exit 1
+    ;;
+  esac
+done
+shift $(($OPTIND - 1))
+
+[ ! -f "$BASEDIR/configure" ] && $BASEDIR/autogen.sh
+
+mkdir -p $BUILDDIR
+cd $BUILDDIR
+
+$BASEDIR/configure --enable-docs
+
+make -j $(nproc) html
+make -j $(nproc) pdf
+
+mv doc/html $PUBLISHDIR
+cp doc/latex/linuxusermodescheduling.pdf $PUBLISHDIR
